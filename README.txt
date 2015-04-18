4da4a8a63209aa7b3a1b700d94d267c1647ff93b
Quincy Reid <qreid51@gmail.com>
2015-04-17 23:15:52 -0400

    Initial commit.

---
+++ README.txt
@@ -0,0 +1,41 @@
+CONTENTS OF THIS FILE
+---------------------
+
+ * Introduction
+ * Requirements
+ * Installation
+ * Usage
+
+INTRODUCTION
+------------
+
+Current Maintainers:
+
+ * Devin Carlson <http://drupal.org/user/290182>
+
+Media: Facebook adds Facebook as a supported media provider.
+
+REQUIREMENTS
+------------
+
+Media: Facebook has one dependency.
+
+Contributed modules
+ * Media Internet - A submodule of the Media module.
+
+INSTALLATION
+------------
+
+Media: Facebook can be installed via the standard Drupal installation process
+(http://drupal.org/node/895232).
+
+USAGE
+-----
+
+Media: Facebook integrates the Facebook video-sharing service with the Media module to
+allow users to add and manage Facebook videos as they would any other piece of
+media.
+
+Internet media can be added on the Web tab of the Add file page (file/add/web).
+With Media: Facebook enabled, users can add a Facebook video by entering its URL or
+embed code.
---
+++ themes/media-facebook-video.tpl.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * @file media_facebook/themes/media-facebook-video.tpl.php
+ *
+ * Template file for theme('media_facebook_video').
+ *
+ * Variables available:
+ *  $uri - The media uri for the Facebook video (e.g., facebook://v/xsy7x8c9).
+ *  $video_id - The unique identifier of the Facebook video (e.g., xsy7x8c9).
+ *  $id - The file entity ID (fid).
+ *  $url - The full url including query options for the Facebook iframe.
+ *  $options - An array containing the Media Facebook formatter options.
+ *  $api_id_attribute - An id attribute if the Javascript API is enabled;
+ *  otherwise NULL.
+ *  $width - The width value set in Media: Facebook file display options.
+ *
+ */
+
+?>
+
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.3";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+<div class="<?php print $classes; ?> media-facebook-<?php print $id; ?>">
+  <div class="fb-video" data-allowfullscreen="true" data-href="<?php print $url; ?>">
+  <div class="fb-xfbml-parse-ignore">
+</div>
